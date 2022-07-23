f4a98f092944f324a7b4cf066799dedf2012799a
Esteban <n4b3ts3@gmail.com>
2022-07-23 15:49:09 -0400

    Added a second test to test if the same user can generate to jsons in the same folder

--- tests.py
+++ tests.py
@@ -7,36 +7,36 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class GDNTest(TestCase):
     def setUp(self, *args, **kwargs):
-        gdnotify = GoDjangoNotify.objects.create(
+        self.gdnotify = GoDjangoNotify.objects.create(
             dependant=User.objects.create(
                 username="test",
                 password="test"
             )
         )
-        GoDjangoNotifyFolders.objects.create(gdn=gdnotify, folder="one_folder", is_enabled=True)
+        self.folders = GoDjangoNotifyFolders.objects.create(gdn=self.gdnotify, folder="one_folder", is_enabled=True)
     
     def test_gdn_push(self, *args, **kwargs):
-        user = User.objects.filter(username="test").first()
-        gdnotify = GoDjangoNotify.objects.filter(dependant=user).first()
-        gdnotify.push(data={
+        self.gdnotify.push(data={
             "tel": "+46764039298",
             "addr": "Mi direccion",
             "msg": "Una cosa",
             "date": "now"
-        }, name=user.username)
+        }, name=self.gdnotify.dependant.username)
+        self.gdnotify.push(data={
+            "tel": "+46764039298",
+            "addr": "Mi direccion",
+            "msg": "Otra cosa",
+            "date": "now"
+        }, name=self.gdnotify.dependant.username)
 
     def test_gdn_tfm_push(self, *args, **kwargs):
-        user = User.objects.filter(username="test").first()
-        gdnotify = GoDjangoNotify.objects.filter(dependant=user).first()
-        gdnotify.push(data={
+        self.gdnotify.push(data={
             "tel": "+46764039298",
             "addr": "Mi direccion",
             "msg": "Una cosa",
             "date": "now",
             "tfm": "something",
-        }, name=user.username)
+        }, name=self.gdnotify.dependant.username)
 
     def test_gdn_clear(self, *args, **kwargs):
-        user = User.objects.filter(username="test").first()
-        gdnotify = GoDjangoNotify.objects.filter(dependant=user).first()
-        gdnotify.godjangonotifyfolders_set.first().clear()
+        self.gdnotify.godjangonotifyfolders_set.first().clear()
